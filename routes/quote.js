585f2fd66368b0c0fc01fb4abeb8164de6f0f2b1
HyunWoo Jung <chopin_liszt@naver.com>
2020-09-21 00:29:41 +0900

    isAuth middleware added

--- routes/quote.js
+++ routes/quote.js
@@ -1,9 +1,7 @@
 const path = require('path');
-
 const express = require('express');
-
 const quoteController = require('../controllers/quote');
-// const isAuth = require('../middleware/is-auth');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
@@ -13,14 +11,14 @@ router.get('/quotes-list/:quoteId', quoteController.getQuote);
 
 router.get('/quotes-list', quoteController.getQuotesList);
 
-router.get('/add-quote', quoteController.getAddQuote);
+router.get('/add-quote', isAuth, quoteController.getAddQuote);
 
-router.post('/add-quote', quoteController.postAddQuote);
+router.post('/add-quote', isAuth, quoteController.postAddQuote);
 
-router.get('/edit-quote/:quoteId', quoteController.getEditQuote);
+router.get('/edit-quote/:quoteId', isAuth, quoteController.getEditQuote);
 
-router.post('/edit-quote', quoteController.postEditQuote);
+router.post('/edit-quote', isAuth, quoteController.postEditQuote);
 
-router.get('/search-quotes', quoteController.getSearchQuotes);
+router.get('/search-quotes', isAuth, quoteController.getSearchQuotes);
 
 module.exports = router;
